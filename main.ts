2d6e363787e5d73a283efc273fa13612bf4ea60b
mql0n <133231446+mql0n@users.noreply.github.com>
2023-05-11 21:09:17 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+let start_A = 0
+let start_b = 0
+input.onButtonPressed(Button.A, function () {
+    start_A = input.runningTime()
+    if (input.buttonIsPressed(Button.A)) {
+    	
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    start_b = 0
+})
